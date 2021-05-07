a91a58ef77a3193145d44b044dbeb725077a7ec9
joutzu <joutzu@gmail.com>
2021-05-07 15:28:42 +0800

    加回後端被刪掉的收藏功能

--- back/server.js
+++ back/server.js
@@ -263,6 +263,32 @@ app.get('/BackStage/SellerPageHero',function(req,res){
   })
 })
 
+// 撈使用者的收藏清單 20200507 Jou
+app.post('/likeproduct', function (req, res){
+  conn.query('select productId from wishproduct where memberId = ?',
+  [req.body.memberId], function (err, result) {
+    if (err) console.log(err)
+    res.send(result)
+  })
+})
+
+// 收藏/取消收藏 20200507 Jou
+app.post('/collectproduct', function (req, res){
+  if (req.body.collect == 'true') {
+    conn.query('insert into `wishproduct` (`memberId`, `productId`) values (?, ?)',
+    [req.body.memberId, req.body.productId], function (err, result){
+      if (err) console.log(err)
+      res.send('收藏成功')
+    })
+  } else{
+    conn.query('delete from `wishproduct` where `memberId` = ? and `productId` = ? '
+    ,[req.body.memberId, req.body.productId], function (err, result){
+      if (err) console.log(err)
+      res.send('取消收藏')
+    })
+  }
+})
+
 //-----------------------------post方法------------------------
 // app.post('/search',function(req,res){
 //     let test = req.body.test
